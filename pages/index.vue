9193b6afb93d35d912da0cd9b2cb0e1577d38171
Ghazi Muharam <ghazimuharam22@gmail.com>
2020-07-19 21:49:46 +0700

    added date functionality

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,13 @@
       <card>
         <div id="header" class="flex justify-between items-center mb-10">
           <div id="date" class="flex items-center">
-            <span class="text-5xl mr-2 font-bold">12</span>
+            <span class="text-5xl mr-2 font-bold">{{ time[0] }}</span>
             <div class="flex flex-col">
-              <span class="font-medium">JAN</span>
-              <span>2016</span>
+              <span class="font-medium">{{ long[0][time[1]] }}</span>
+              <span>{{ time[2] }}</span>
             </div>
           </div>
-          <span class="uppercase font-medium">Tuesday</span>
+          <span class="uppercase font-medium">{{ long[1][time[3]] }}</span>
         </div>
         <todo-add @addTodo="addTodo" />
         <div id="body" class="pb-10">
@@ -33,8 +33,17 @@
 export default {
   data () {
     return {
-      todos: []
+      todos: [],
+      long: [['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Ssep', 'Oct', 'Nov', 'Dec'], ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday', 'Saturday']],
+      time: []
     }
+  },
+  mounted () {
+    const theDate = new Date()
+    this.time.push(theDate.getDate())
+    this.time.push(theDate.getMonth())
+    this.time.push(theDate.getFullYear())
+    this.time.push(theDate.getDay())
   },
   methods: {
     addTodo (data) {
